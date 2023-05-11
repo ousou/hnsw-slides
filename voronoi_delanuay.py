898de2fcbc007e08c0b1c347e95be28be45bd9db
Sebastian Björkqvist <sebastian@iprally.com>
2023-05-11 15:29:08 +0300

    Fixed greedy search in script

--- voronoi_delanuay.py
+++ voronoi_delanuay.py
@@ -28,6 +28,8 @@ graph = {}
 for i in range(points.shape[0]):
     graph[i] = set(tri.vertex_neighbor_vertices[1][tri.vertex_neighbor_vertices[0][i]:tri.vertex_neighbor_vertices[0][i+1]])
 
+edge_count = sum(len(graph[i]) for i in graph) / 2
+print("edge_count", edge_count)
 
 # greedy search algorithm
 def greedy_search(graph, points, start, end):
@@ -41,6 +43,22 @@ def greedy_search(graph, points, start, end):
                 total_cost = cost + np.linalg.norm(points[neighbor] - points[end])
                 heapq.heappush(heap, (total_cost, neighbor, path + [neighbor]))
 
+def greedy_search_recursive(graph, points, start, end):
+    return _greedy_search_2(graph, points, [start], end)
+
+def _greedy_search_2(graph, points, current_path, end):
+    if current_path[-1] == end:
+        return current_path
+    closest_neighbor = None
+    for neighbor in graph[current_path[-1]]:
+        if neighbor not in current_path:
+            if closest_neighbor is None:
+                closest_neighbor = neighbor
+            elif np.linalg.norm(points[neighbor] - points[end]) < np.linalg.norm(points[closest_neighbor] - points[end]):
+                closest_neighbor = neighbor
+    current_path.append(closest_neighbor)
+    return _greedy_search_2(graph, points, current_path, end)
+
 # find pair of points for which greedy search algorithm returns longest path
 pairs_to_try = 50
 random_pairs = np.random.randint(0, len(points), (pairs_to_try, 2))
@@ -48,11 +66,15 @@ longest_path = []
 longest_path_start = -1
 longest_path_end = -1
 for i,j in random_pairs:
-    path = greedy_search(graph, points, i, j)
+    path = greedy_search_recursive(graph, points, i, j)
     if len(path) > len(longest_path):
         longest_path = path
         longest_path_start = i
         longest_path_end = j
+
+print("longest_path_start", longest_path_start)
+print("longest_path_end", longest_path_end)
+print("longest_path", longest_path)
 
 plt.savefig("delaunay_triangulation_256_points.svg")
 # plot longest path
@@ -65,7 +87,7 @@ plt.plot(longest_path_points[:, 0], longest_path_points[:, 1], 'r-', linewidth=4
 plt.savefig("delaunay_triangulation_longest_path.svg")
 #plt.show()
 
-random_edge_count = 16
+random_edge_count = 32
 random_edges = np.random.randint(0, len(points), (random_edge_count, 2))
 random_edge_points = points[random_edges]
 #plt.triplot(points[:,0], points[:,1], tri.simplices.copy())
@@ -74,12 +96,15 @@ plt.plot(random_edge_points.T[0], random_edge_points.T[1], 'y-')
 plt.savefig("delaunay_triangulation_random_edges.svg")
 
 graph_with_random_edges = deepcopy(graph)
+print("random_edges", random_edges)
 
 for i,j in random_edges:
     graph_with_random_edges[i].add(j)
     graph_with_random_edges[j].add(i)
 
-new_path = greedy_search(graph_with_random_edges, points, longest_path_start, longest_path_end)
+
+new_path = greedy_search_recursive(graph_with_random_edges, points, longest_path_start, longest_path_end)
+print("new_path", new_path)
 new_path_points = np.array([points[i] for i in new_path])
 #plt.plot(longest_path_points[:, 0], longest_path_points[:, 1], 'r-', linewidth=4.0)
 #plt.plot(longest_path_end_points[:,0], longest_path_end_points[:,1], 'o', color='red')
